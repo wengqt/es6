f9e536a7631426bbabb7346848c1c4c2e83dce5a
weng <1906338209@qq.com>
2016-12-07 23:46:24 +0800

    12.7

---
+++ es-compiled.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Created by weng on 2016/12/7.
+ */
+
+input.map(function (item) {
+  return item + 1;
+});
+
+//# sourceMappingURL=es-compiled.js.map
---
+++ es.js
@@ -0,0 +1,6 @@
+/**
+ * Created by weng on 2016/12/7.
+ */
+
+input.map(item=>item+1);
+
